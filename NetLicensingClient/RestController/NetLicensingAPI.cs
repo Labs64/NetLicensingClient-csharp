86fc37a8f48d2d237fd41d5bc65f741d20877683
ayushkevich <yushkevich@me.com>
2019-01-11 17:24:29 +0300

    change protocol to https in userAgent header

--- NetLicensingClient/RestController/NetLicensingAPI.cs
+++ NetLicensingClient/RestController/NetLicensingAPI.cs
@@ -64,7 +64,7 @@ namespace NetLicensingClient.RestController
             }
 
             HttpWebRequest request = WebRequest.Create(context.baseUrl + "/" + path + urlParam) as HttpWebRequest;
-            request.UserAgent = "NetLicensing/C# " + System.Environment.Version + " (http://netlicensing.io)";
+            request.UserAgent = "NetLicensing/C# " + System.Environment.Version + " (https://netlicensing.io)";
 
             switch (method)
             {
