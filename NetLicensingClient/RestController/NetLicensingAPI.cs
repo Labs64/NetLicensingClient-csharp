726af9a6fa7c74efa3dcc136231c14fc9ae8f120
Alexey Averikhin <alexey.averikhin@labs64.de>
2020-09-07 15:18:29 +0200

    Test .NET Standard with no dependencies

--- NetLicensingClient/RestController/NetLicensingAPI.cs
+++ NetLicensingClient/RestController/NetLicensingAPI.cs
@@ -8,11 +8,6 @@ using System.Text.RegularExpressions;
 using System.Web;
 using NetLicensingClient.Entities;
 using NetLicensingClient.Exceptions;
-using Org.BouncyCastle.Crypto.Parameters;
-using Org.BouncyCastle.OpenSsl;
-using System.Security.Cryptography;
-using System.Security.Cryptography.Xml;
-using System.Xml;
 
 namespace NetLicensingClient.RestController
 {
@@ -137,10 +132,6 @@ namespace NetLicensingClient.RestController
                                 using (StreamReader reader = new StreamReader(memoryStream))
                                 {
                                     var responseString = reader.ReadToEnd();
-                                    if (!VerifyXmlSignature(responseString, context.publicKey))
-                                    {
-                                        throw new NetLicensingException("XML signature could not be verified");
-                                    }
                                 }
                             }
                             memoryStream.Dispose();
@@ -207,48 +198,6 @@ namespace NetLicensingClient.RestController
             return NetLicensingSerializer.Deserialize(responseStream) as netlicensing;
         }
 
-		private static bool VerifyXmlSignature(string xmlString, string publicKey)
-		{
-			using (var keyReader = new StringReader(publicKey))
-			{
-				var pemReader = new PemReader(keyReader);
-
-				RsaKeyParameters parameters = (RsaKeyParameters)pemReader.ReadObject();
-				RSAParameters rParams = new RSAParameters();
-				rParams.Modulus = parameters.Modulus.ToByteArray();
-				rParams.Exponent = parameters.Exponent.ToByteArray();
-
-                RSA rsaKey = RSA.Create();
-                rsaKey.ImportParameters(rParams);
-
-				XmlDocument xmlDoc = new XmlDocument();
-				xmlDoc.PreserveWhitespace = true;
-				xmlDoc.LoadXml(xmlString);
-
-                // Create a new SignedXml object and pass it the XML document class
-                SignedXml signedXml = new SignedXml(xmlDoc);
-                // Find the "Signature" node and create a new XmlNodeList object
-                XmlNodeList nodeList = xmlDoc.GetElementsByTagName("Signature");
-
-                // Throw an exception if no signature was found
-                if (nodeList.Count <= 0)
-				{
-					throw new CryptographicException("Verification failed: No Signature was found in the document.");
-				}
-
-				// Throw an exception if more than one signature was found
-				if (nodeList.Count >= 2)
-				{
-					throw new CryptographicException("Verification failed: More that one signature was found for the document.");
-				}
-
-                // Load the first <signature> node
-                signedXml.LoadXml((XmlElement)nodeList[0]);
-
-                // Check the signature and return the result
-                return signedXml.CheckSignature(rsaKey);
-			}
-		}
     }
 
 }
